6371e759da566b0f929e679931549d9f8c2bd065
Nocillax <asifjarif@gmail.com>
2025-10-04 12:48:09 +0600

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      process.env.FRONTEND_URL || "http://localhost:5173",
+      "https://nocillax-it-ums.vercel.app"
+    ],
     credentials: true,
   })
 );
